7be98590805a2ef800ee2176d7e7a039b7f41862
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-12-06 08:08:56 +0200

    feat: add drizzle orm

--- packages/server/src/app/config.ts
+++ packages/server/src/app/config.ts
@@ -3,10 +3,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export type AppConfig = {
+  databaseUrl: string
   port: number
 }
 
 const appConfig: AppConfig = {
+  databaseUrl:
+    process.env.DATABASE_URL ||
+    'postgres://postgres:postgres@localhost:5432/development',
   port: Number(process.env.PORT) || 3000,
 }
 
